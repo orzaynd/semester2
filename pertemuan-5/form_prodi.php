cc95763f43f4c35d84a3d61b010c75b547a298a1
Oryza <caramellatez.asli@gmail.com>
2025-04-08 20:03:54 +0700

    baru

---
+++ pertemuan-5/form_prodi.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form method="POST" action="proses_prodi.php">
+  <div class="form-group row">
+    <label for="text" class="col-4 col-form-label">kode</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="text" name="text" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text2" class="col-4 col-form-label">Nama Prodi</label> 
+    <div class="col-8">
+      <input id="text2" name="text2" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text3" class="col-4 col-form-label">Kepala Prodi</label> 
+    <div class="col-8">
+      <input id="text3" name="text3" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <input name="proses" type="submit" value="simpan" class="btn btn-primary">
+    </div>
+  </div>
+</form>
